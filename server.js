757bf9d4e4b1f525a0f7d8ef4d87786f6e702808
C4Jay <choolakejay@gmail.com>
2019-11-05 21:20:53 +0530

    server1

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true , useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('db running'))
 
 app.listen(3000, () => console.log('Server running'))
